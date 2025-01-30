8a534822db38a070bba7f3cbeb0edb4cf85dc860
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-30 14:27:44 +0530

    Create package.js

---
+++ app/patient-service/package.js
@@ -0,0 +1,12 @@
+{
+  "name": "patient-service",
+  "version": "1.0.0",
+  "description": "A microservice for managing patients",
+  "main": "patient-service.js",
+  "scripts": {
+    "start": "node patient-service.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
